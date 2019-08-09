78418c30e6b4b11bc22f2017ca16d1c319f1f5a2
Aida Yrysbekova <aidiko20@gmail.com>
2019-08-08 22:34:24 -0400

    connected jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Sladkiy2216",
-  database: "burger_db"
-});
-
+var connection;
+if (process.env.JAWSBD_URL) {
+  connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Sladkiy2216",
+    database: "burger_db"
+  });
+};
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var burger = require("../models/burger.js");
+var burger = requirse("../models/burger.js");
 
 router.get("/", function (req, res) {
     burger.all(function(data) {
